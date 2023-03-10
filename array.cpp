6d4bdb3e386921e7d168e4ca0c34ab3621448cd2
VisheshRawal <vp2005rawal@gmail.com>
2023-03-10 18:27:07 +0530

    arrays

---
+++ array.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int getMax(){
+    
+}
+
+int main()
+{
+    int size;
+    cin >> size;
+
+    int num[10];
+
+    for(int i= 0; i< size; i++){
+        cin >> num[i];
+    }
+
+    getMax();
+}
